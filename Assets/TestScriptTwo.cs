dfe862fb78b96f594aa8f5b731925d269ee7c132
sjkjsjkj <sjhh64@gmail.com>
2026-03-07 14:24:41 +0900

    test Commit
    스크립트two 수정

--- Assets/TestScriptTwo.cs
+++ Assets/TestScriptTwo.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class TestScriptTwo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //감격의 첫 수정입니다.
 }
