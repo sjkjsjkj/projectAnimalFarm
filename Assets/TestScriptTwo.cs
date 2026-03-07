49e6849d3bfda5a3ceec5a4698af22a1d347baba
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-03-07 14:37:39 +0900

    initial commit

--- Assets/TestScriptTwo.cs
+++ Assets/TestScriptTwo.cs
@@ -7,7 +7,7 @@ public class TestScriptTwo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        dsad
+        
     }
 
     // Update is called once per frame
