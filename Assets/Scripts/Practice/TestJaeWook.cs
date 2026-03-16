ac0bcf27daebae3721744a79817c460f1969dc54
sjkjsjkj <sjhh64@gmail.com>
2026-03-16 16:22:21 +0900

    test : 스크립트 추가
    testJaeWook 수정

--- Assets/Scripts/Practice/TestJaeWook.cs
+++ Assets/Scripts/Practice/TestJaeWook.cs
@@ -7,7 +7,7 @@ public class TestJaeWook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Test 16:10
+        //Test 16:22
     }
 
     // Update is called once per frame
