3f7cd55ce0fc15012eb8d44aa756ff044fc734d9
wonyong27-hub <wonyong27@gmail.com>
2026-03-18 10:36:43 +0900

    test

---
+++ Assets/Scenes/CWY/Tast.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tast : MonoBehaviour
+{
+
+    #region ─────────────────────────▶ 인스펙터 ◀─────────────────────────
+    
+    #endregion
+
+    #region ─────────────────────────▶ 접근자 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 중첩 타입 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 내부 변수 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 내부 메서드 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 외부 메서드 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 메시지 함수 ◀─────────────────────────
+
+    #endregion
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
