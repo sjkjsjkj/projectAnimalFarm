141e599be843323ee2468181283b4271b56876d0
ryuthank91-blip <ryuthank91@gmail.com>
2026-03-30 16:52:39 +0900

    feat : 인벤토리 생성
    I키 열고닫기
    X버튼 닫기
    드래그 앤 드롭 슬롯 이동
    우클릭 Use / Trash / Cancel 메뉴
    15칸 그리드 인벤토리

---
+++ Assets/Scenes/SRJ/InventoryManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+
+    [Header("UI")]
+    public GameObject inventoryPanel;
+    public Transform slotContainer;
+    public GameObject slotPrefab;
+
+    [Header("Settings")]
+    public int totalSlots = 15;
+
+    [Header("Test Items")]
+    public List<ItemData> testItems;
+
+    private List<InventorySlot> slots = new List<InventorySlot>();
+    private bool isOpen = false;
+
+    void Awake() => Instance = this;
+
+    void Start()
+    {
+        InitSlots();
+        AddTestItems();
+        inventoryPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            ToggleInventory();
+    }
+
+    void InitSlots()
+    {
+        for (int i = 0; i < totalSlots; i++)
+        {
+            GameObject go = Instantiate(slotPrefab, slotContainer);
+            InventorySlot slot = go.GetComponent<InventorySlot>();
+            slots.Add(slot);
+        }
+    }
+
+    void AddTestItems()
+    {
+        foreach (var item in testItems)
+            AddItem(item);
+    }
+
+    public bool AddItem(ItemData item)
+    {
+        foreach (var slot in slots)
+        {
+            if (slot.IsEmpty)
+            {
+                slot.SetItem(item);
+                return true;
+            }
+        }
+        Debug.Log("인벤토리가 가득 찼습니다!");
+        return false;
+    }
+
+    public void RemoveItem(InventorySlot slot)
+    {
+        slot.ClearSlot();
+    }
+
+    public void ToggleInventory()
+    {
+        isOpen = !isOpen;
+        inventoryPanel.SetActive(isOpen);
+    }
+
+    public void CloseInventory()
+    {
+        isOpen = false;
+        inventoryPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scenes/SRJ/TooltipUI.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TooltipUI : MonoBehaviour
+{
+    public static TooltipUI Instance;
+
+    [Header("UI References")]
+    public GameObject tooltipPanel;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI typeText;
+    public TextMeshProUGUI descText;
+
+    private RectTransform rectTransform;
+    private Canvas rootCanvas;
+
+    void Awake()
+    {
+        Instance = this;
+        rectTransform = tooltipPanel.GetComponent<RectTransform>();
+        rootCanvas = GetComponentInParent<Canvas>();
+        tooltipPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (tooltipPanel.activeSelf)
+            MoveToMouse();
+    }
+
+    public void Show(ItemData item)
+    {
+        nameText.text = item.itemName;
+        typeText.text = $"[{item.itemType}]";
+        descText.text = item.description;
+        tooltipPanel.SetActive(true);
+        MoveToMouse();
+    }
+
+    public void Hide()
+    {
+        tooltipPanel.SetActive(false);
+    }
+
+    void MoveToMouse()
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            rootCanvas.GetComponent<RectTransform>(),
+            Input.mousePosition,
+            rootCanvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : rootCanvas.worldCamera,
+            out pos
+        );
+
+        // 화면 밖으로 안 나가게 오프셋 조정
+        float offsetX = 15f;
+        float offsetY = -15f;
+
+        Vector2 size = rectTransform.sizeDelta;
+        float canvasW = rootCanvas.GetComponent<RectTransform>().rect.width;
+        float canvasH = rootCanvas.GetComponent<RectTransform>().rect.height;
+
+        if (pos.x + size.x + offsetX > canvasW / 2) offsetX = -size.x - 15f;
+        if (pos.y + offsetY - size.y < -canvasH / 2) offsetY = size.y + 15f;
+
+        rectTransform.anchoredPosition = new Vector2(pos.x + offsetX, pos.y + offsetY);
+    }
+}
