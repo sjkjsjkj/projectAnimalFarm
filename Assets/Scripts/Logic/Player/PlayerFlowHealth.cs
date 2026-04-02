f5c7acba0450bd8ddaa1dac5bc535d0dac9f2c5f
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-04-02 15:01:14 +0900

    fix : UI 카메라가 메인 맵을 비추지 않도록 수정
    → 메인 카메라와 UI 카메라의 컬링 레이어 조정

---
+++ Assets/Scripts/Logic/Player/PlayerFlowHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 플레이어 체력
+/// </summary>
+public class PlayerFlowHealth : BaseMono
+{
+    #region ─────────────────────────▶ 인스펙터 ◀─────────────────────────
+    //[Header("주제")]
+    //[SerializeField] private Class _class;
+    #endregion
+
+    #region ─────────────────────────▶ 내부 변수 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 공개 멤버 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 내부 메서드 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 메시지 함수 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 중첩 타입 ◀─────────────────────────
+
+    #endregion
+}
