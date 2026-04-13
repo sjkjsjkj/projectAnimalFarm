ca012c834fb7a0c46fb9ba513b82a87feb96b1fa
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-04-14 02:15:56 +0900

    feat : 낮과 밤에 따른 밝기 변화 구현
    → 하루 600초
    → 마을이나 숲에서 밤일 경우 BGM 변경
    → 시간에 따른 밝기 변화

---
+++ Assets/Scripts/Global/EventBus/Record/OnInventoryOpen.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// 인벤토리를 열었을 때
+/// </summary>
+public readonly struct OnInventoryOpen
+{
+    public static void Publish()
+    {
+        EventBus<OnInventoryOpen>.Publish(new());
+    }
+}
