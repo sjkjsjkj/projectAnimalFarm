445c6ffbae0d9083d884cea5107cfc3401689a19
sjkjsjkj <sjhh64@gmail.com>
2026-04-16 15:16:28 +0900

    feat : 소모품 상점 기능 추가

---
+++ Assets/Scripts/Data/Item/Consumable/ConsumeItemSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 소모품 SO
+/// </summary>
+[CreateAssetMenu(fileName = "ConsumeItemSO_", menuName = "ScriptableObjects/Consume", order = 1)]
+
+public class ConsumeItemSO : ItemSO
+{
+
+}
---
+++ Assets/Scripts/Data/Item/ItemUseContext/ItemUseRecovery.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// SO 클래스의 설계 의도입니다.
+/// </summary>
+[CreateAssetMenu(fileName = "ItemUseRecovery_", menuName = "ScriptableObjects/ItemUseContext/Recovery", order = 1)]
+public class ItemUseRecovery : ItemUseContextSO
+{
+    #region ─────────────────────────▶ 인스펙터 ◀─────────────────────────
+    [Header("기본 정보")]
+    [SerializeField] protected int _hungerRecoveryAmount;
+    [SerializeField] protected int _thirstRecoveryAmount;
+
+    #endregion
+
+    #region ─────────────────────────▶ 공개 멤버 ◀─────────────────────────
+    public int HungerRecoveryAmount => _hungerRecoveryAmount;
+    public int ThirstRecoveryAmount => _thirstRecoveryAmount;
+    public override bool TryUse()
+    {
+        DataManager.Ins.Player.RecoverHunger(_hungerRecoveryAmount);
+        DataManager.Ins.Player.RecoverThirst(_thirstRecoveryAmount);
+        return true;
+    }
+    #endregion
+
+    #region ─────────────────────────▶ 메시지 함수 ◀─────────────────────────
+  
+    #endregion
+}
