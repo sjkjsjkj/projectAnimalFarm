6e4c2ed44ded06511f91c71d1fe4408095b75025
sjkjsjkj <sjhh64@gmail.com>
2026-04-16 11:58:22 +0900

    fix : 인벤토리에서 Trash 버튼이 동작하지 않던 이슈 수정

--- Assets/Scripts/Global/Define/KDefine.Inventory.cs
+++ Assets/Scripts/Global/Define/KDefine.Inventory.cs
@@ -2,7 +2,7 @@
 
 public static partial class K
 {
-    public static readonly int PLAYER_INVENTORY_SIZE = 15;
-    public static readonly int STORAGE_INVENTORY_SIZE = 15;
+    public static readonly int PLAYER_INVENTORY_SIZE = 30;
+    public static readonly int STORAGE_INVENTORY_SIZE = 50;
     public static readonly int FOODBOX_INVENTORY_SIZE = 5;
 }
