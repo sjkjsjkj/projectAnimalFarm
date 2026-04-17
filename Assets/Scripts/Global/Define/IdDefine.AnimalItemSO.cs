a1cc322439670c837393e9890e170deeabac86b2
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-04-17 13:58:23 +0900

    fix : 동적 해상도 대응을 위한 대부분 Ui 수정
    → 앵커, 리소스 로드 코드 등

---
+++ Assets/Scripts/Global/Define/IdDefine.AnimalItemSO.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable IDE1006
+
+public static partial class Id
+{
+    public const string Item_Animal_Chicken = "Item_Animal_Chicken";
+    public const string Item_Animal_Cow = "Item_Animal_Cow";
+    public const string Item_Animal_Duck = "Item_Animal_Duck";
+    public const string Item_Animal_Goat = "Item_Animal_Goat";
+    public const string Item_Animal_Horse = "Item_Animal_Horse";
+    public const string Item_Animal_Ostrich = "Item_Animal_Ostrich";
+    public const string Item_Animal_Pig = "Item_Animal_Pig";
+    public const string Item_Animal_Sheep = "Item_Animal_Sheep";
+}
